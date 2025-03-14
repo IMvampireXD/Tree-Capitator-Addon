ce103ea19be866f88208dc8c1bb7cc6eeec1463e
IMvampireXD <118794023+IMvampireXD@users.noreply.github.com>
2025-03-14 09:36:52 -0700

    Create main.js

---
+++ main.js
@@ -0,0 +1,61 @@
+import { world, system } from '@minecraft/server';
+
+const woodBlocks = new Set([
+    'minecraft:oak_log',
+    'minecraft:spruce_log',
+    'minecraft:birch_log',
+    'minecraft:jungle_log',
+    'minecraft:acacia_log',
+    'minecraft:dark_oak_log',
+    'minecraft:mangrove_log',
+    'minecraft:cherry_log',
+    'minecraft:crimson_stem',
+    'minecraft:warped_stem',
+    'minecraft:oak_leaves',
+    'minecraft:spruce_leaves',
+    'minecraft:birch_leaves',
+    'minecraft:jungle_leaves',
+    'minecraft:acacia_leaves',
+    'minecraft:dark_oak_leaves',
+    'minecraft:mangrove_leaves',
+    'minecraft:cherry_leaves'
+]);
+
+world.beforeEvents.playerBreakBlock.subscribe(e => {
+    const { block, player } = e;
+    const hand = player.getComponent('minecraft:equippable').getEquipmentSlot('Mainhand');
+    const lore = hand.getLore();
+    const veinminer = lore?.includes('Treecapitator') && woodBlocks.has(block.typeId);
+
+    if (veinminer) {
+        let dimension = block.dimension;
+        let toBreak = [block.location];
+        let checked = new Set();
+
+        while (toBreak.length > 0) {
+            let location = toBreak.shift();
+            let key = `${location.x},${location.y},${location.z}`;
+            if (checked.has(key)) continue;
+            checked.add(key);
+
+            let currentBlock = dimension.getBlock(location);
+            if (currentBlock && woodBlocks.has(currentBlock.typeId)) {
+                system.run(() => {
+                    dimension.runCommand(`setblock ${location.x} ${location.y} ${location.z} air destroy`);
+                });
+                let adjacent = [
+                    { x: location.x + 1, y: location.y, z: location.z },
+                    { x: location.x - 1, y: location.y, z: location.z },
+                    { x: location.x, y: location.y + 1, z: location.z },
+                    { x: location.x, y: location.y - 1, z: location.z },
+                    { x: location.x, y: location.y, z: location.z + 1 },
+                    { x: location.x, y: location.y, z: location.z - 1 }
+                ];
+                for (let loc of adjacent) {
+                    toBreak.push(loc);
+                }
+            }
+        }
+        e.cancel = false;
+    }
+});
