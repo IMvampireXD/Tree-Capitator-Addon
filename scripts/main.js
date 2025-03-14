6fc457e76d20c2949f3bc8cd49f015339edc66bd
CennacEh <cennacdude@gmail.com>
2025-03-14 18:32:50 +0000

    Added runJob for better preformance

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,4 @@
-import { world, system } from '@minecraft/server';
+import { world, system, Dimension, Block } from '@minecraft/server';
 
 const woodBlocks = new Set([
     'minecraft:oak_log',
@@ -11,14 +11,6 @@ const woodBlocks = new Set([
     'minecraft:cherry_log',
     'minecraft:crimson_stem',
     'minecraft:warped_stem',
-    'minecraft:oak_leaves',
-    'minecraft:spruce_leaves',
-    'minecraft:birch_leaves',
-    'minecraft:jungle_leaves',
-    'minecraft:acacia_leaves',
-    'minecraft:dark_oak_leaves',
-    'minecraft:mangrove_leaves',
-    'minecraft:cherry_leaves'
 ]);
 
 world.beforeEvents.playerBreakBlock.subscribe(e => {
@@ -28,35 +20,48 @@ world.beforeEvents.playerBreakBlock.subscribe(e => {
     const lore = hand.getLore();
     const veinminer = lore?.includes('Treecapitator') && woodBlocks.has(block.typeId);
 
-    if (veinminer) {
-        let dimension = block.dimension;
-        let toBreak = [block.location];
-        let checked = new Set();
-
-        while (toBreak.length > 0) {
-            let location = toBreak.shift();
-            let key = `${location.x},${location.y},${location.z}`;
-            if (checked.has(key)) continue;
-            checked.add(key);
-
-            let currentBlock = dimension.getBlock(location);
-            if (currentBlock && woodBlocks.has(currentBlock.typeId)) {
-                system.run(() => {
-                    dimension.runCommand(`setblock ${location.x} ${location.y} ${location.z} air destroy`);
-                });
-                let adjacent = [
-                    { x: location.x + 1, y: location.y, z: location.z },
-                    { x: location.x - 1, y: location.y, z: location.z },
-                    { x: location.x, y: location.y + 1, z: location.z },
-                    { x: location.x, y: location.y - 1, z: location.z },
-                    { x: location.x, y: location.y, z: location.z + 1 },
-                    { x: location.x, y: location.y, z: location.z - 1 }
-                ];
-                for (let loc of adjacent) {
-                    toBreak.push(loc);
-                }
-            }
+    if (!veinminer) {
+        const dimension = block.dimension;
+        if (woodBlocks.has(block.typeId)) {
+            system.runJob(breakTree(dimension, block));
+            e.cancel = true
         }
-        e.cancel = false;
     }
 });
+
+
+/**
+ * 
+ * @param {Dimension} dimension 
+ * @param {Block} block 
+ */
+function* breakTree(dimension, block) {
+    let toBreak = [block.location];
+    let checked = new Set();
+
+    while (toBreak.length > 0) {
+        let location = toBreak.shift();
+        let key = `${location.x},${location.y},${location.z}`;
+        if (checked.has(key)) continue;
+        checked.add(key);
+
+        let currentBlock = dimension.getBlock(location);
+        if (currentBlock && woodBlocks.has(currentBlock.typeId)) {
+            system.run(() => {
+                dimension.runCommand(`setblock ${location.x} ${location.y} ${location.z} air destroy`);
+            })
+            let adjacent = [
+                { x: location.x + 1, y: location.y, z: location.z },
+                { x: location.x - 1, y: location.y, z: location.z },
+                { x: location.x, y: location.y + 1, z: location.z },
+                { x: location.x, y: location.y - 1, z: location.z },
+                { x: location.x, y: location.y, z: location.z + 1 },
+                { x: location.x, y: location.y, z: location.z - 1 }
+            ];
+            for (let loc of adjacent) {
+                toBreak.push(loc);
+            }
+        }
+        yield;
+    }
+}
